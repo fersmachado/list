5eff48ae9d2e0a10d0d9b8bb43a00066db2a4c9e
Fernanda Machado <102155332+fersmachado@users.noreply.github.com>
2022-08-11 16:51:29 -0300

    feito

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,4 @@
-module.exports {
+module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
@@ -13,4 +13,3 @@ module.exports {
     "builder": "@storybook/builder-webpack5"
   },
 }
-
